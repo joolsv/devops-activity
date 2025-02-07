81a5c8b46838019d5fb8123fdbe36952d4895eeb
joolsv <julsviar@gmail.com>
2025-02-07 07:52:26 +0000

    hoy hoy

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "flask_app" {
   ami           = "ami-085ad6ae776d8f09c"
   instance_type = "t2.micro"
-  key_name      = "jv-key"
+  # key_name      = "jv-key"
 
   tags = {
     Name = "flask-app-instance-juls"
