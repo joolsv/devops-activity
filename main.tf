9b0c8517fbc44d958366c82c8ede340ca46f2ef9
joolsv <julsviar@gmail.com>
2025-02-07 08:28:05 +0000

    ha

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "flask_app" {
   ami           = "ami-085ad6ae776d8f09c"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.flask_key.key_name # Ensure SSH key is used
+  # key_name      = aws_key_pair.flask_key.key_name # Ensure SSH key is used
 
   tags = {
     Name = "flask-app-instance-juls"
