143da43f93e1450bad45f5c45051868889edf284
joolsv <julsviar@gmail.com>
2025-02-07 08:15:33 +0000

    hey

--- main.tf
+++ main.tf
@@ -2,31 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "flask_key" {
-  key_name   = "jv-key"
-  public_key = file("/home/ec2-user/devops-activity/jv-key.pub") # Ensure this file exists
-}
+# resource "aws_key_pair" "flask_key" {
+#  key_name   = "jv-key"
+#  public_key = file("/home/ec2-user/devops-activity/jv-key.pub") # Ensure this file exists
+# }
 
 resource "aws_instance" "flask_app" {
   ami           = "ami-085ad6ae776d8f09c"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.flask_key.key_name # Ensure SSH key is used
+  #  key_name      = aws_key_pair.flask_key.key_name # Ensure SSH key is used
 
   tags = {
     Name = "flask-app-instance-juls"
   }
 
   # Connection block required for provisioners
-  connection {
-    type        = "ssh"
-    user        = "ec2-user" # Default Amazon Linux user
-    private_key = file("/root/.ssh/jv-key") # Ensure this private key exists
-    host        = self.public_ip
+  #  connection {
+  #  type        = "ssh"
+  #  user        = "ec2-user" # Default Amazon Linux user
+  #  private_key = file("/root/.ssh/jv-key") # Ensure this private key exists
+  #  host        = self.public_ip
   }
 
-  provisioner "file" {
-    source      = "../ansible"
-    destination = "/home/ec2-user/"
+  # provisioner "file" {
+  #  source      = "../ansible"
+  #  destination = "/home/ec2-user/"
   }
 }
 
