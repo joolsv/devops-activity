f654e6242fb62fbeb0c94f9a9e3525147b440bac
joolsv <julsviar@gmail.com>
2025-02-07 14:47:31 +0000

    updated config

--- main.tf
+++ main.tf
@@ -11,15 +11,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Lookup existing VPC by ID
 data "aws_vpc" "existing_vpc" {
-  id = "vpc-0ac15a8ab99f47d9e" # Replace with your actual VPC ID
+  id = "vpc-0ac15a8ab99f47d9e"
 }
 
 # Lookup existing Security Group inside the VPC
 data "aws_security_group" "existing_sg" {
   vpc_id = data.aws_vpc.existing_vpc.id
-  name   = "manila-ops-devops-training-internal-and-secure-public" # Replace with actual Security Group name
+  name   = "manila-ops-devops-training-internal-and-secure-public"
 }
 
 # Lookup a specific subnet in us-east-1b
@@ -32,7 +31,7 @@ resource "aws_instance" "flask_app" {
   ami                    = "ami-085ad6ae776d8f09c"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
-  subnet_id              = data.aws_subnet.us_east_1b_subnet.id # Use us-east-1b subnet
+  subnet_id              = data.aws_subnet.us_east_1b_subnet.id
   iam_instance_profile   = "FT-EC2-Role"
 
   tags = {
